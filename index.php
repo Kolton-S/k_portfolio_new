045731df4d1e6a106bd3e893dbeb2f6845347a3a
Simpson <k_simpson22@fanshawec.ca>
2018-01-25 10:54:57 -0500

    added scrolljacking

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Kolton Simpson - Developer</title>
+  <script
+  src="https://code.jquery.com/jquery-3.3.1.min.js"
+  integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
+  crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/gasparesganga-jquery-loading-overlay@1.5.4/src/loadingoverlay.min.js"></script>
+  <script src="js/main.js"></script>
+  <script src="js/jquery.fullpage.js"></script>
+  <link href="css/reset.css" rel="stylesheet" type="text/css">
+  <link href="css/main.css" rel="stylesheet" type="text/css">
+  <link href="css/jquery.fullpage.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+  <div id="fullpage" class="fullpage-wrapper">
+  	<div class="section">Some section</div>
+  	<div class="section">Some section</div>
+  	<div class="section">Some section</div>
+  	<div class="section">Some section</div>
+  </div>
+</body>
+</html>
